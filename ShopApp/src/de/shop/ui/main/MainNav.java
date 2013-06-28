f54c4e9f351ce1ca4cac9daa14da3cb8ee55ee09
Sebastian Braun <Sebastian_Braun89@gmx.de>
2013-06-28 10:32:47 +0200

    Fehler behoben

---
+++ ShopApp/src/de/shop/ui/main/MainNav.java
@@ -0,0 +1,199 @@
+package de.shop.ui.main;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.app.Fragment;
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.PopupMenu;
+import android.widget.PopupMenu.OnMenuItemClickListener;
+import android.widget.SimpleAdapter;
+import de.shop.R;
+import de.shop.ui.artikel.Artikel;
+import de.shop.ui.artikel.ArtikelCreate;
+import de.shop.ui.artikel.ArtikelDelete;
+import de.shop.ui.artikel.ArtikelSuchenArtikelgruppe;
+import de.shop.ui.artikel.ArtikelSuchenBezeichnung;
+import de.shop.ui.artikel.ArtikelSuchenId;
+//import de.shop.ui.kunde.KundeCreate;
+import de.shop.ui.kunde.KundeDelete;
+import de.shop.ui.kunde.KundeSuchenId;
+import de.shop.ui.kunde.Kunden;
+import de.shop.ui.kunde.KundenSucheNachname;
+
+public class MainNav extends ListFragment implements OnItemClickListener, OnMenuItemClickListener {
+	public enum NavType {
+		KUNDEN(0),
+		//BESTELLUNGEN(2),
+		ARTIKEL(1);
+		
+		private int value;
+		
+		private NavType(int value) {
+			this.value = value;
+		}
+		
+		public int getValue() {
+			return value;
+		}
+		
+		public static NavType valueOf(int value) {
+			switch (value) {
+				case 0:	return KUNDEN;
+//				case 2:	return BESTELLUNGEN;
+				case 1: return ARTIKEL;
+				default: return KUNDEN;
+			}
+		}
+	}
+	
+	private static final String LOG_TAG = MainNav.class.getSimpleName();
+	
+	private static final String ICON = "icon";
+	private static final String TEXT = "text";
+	private static final String[] FROM = { ICON, TEXT };
+	private static final int[] TO = { R.id.nav_icon, R.id.nav_text };
+	
+	private PopupMenu kundenPopup;
+	private PopupMenu artikelPopup;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		final ListAdapter listAdapter = createListAdapter();        
+        setListAdapter(listAdapter);
+        
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+	
+	private ListAdapter createListAdapter() {
+		final NavType[] navValues = NavType.values();
+		final List<Map<String, Object>> navList = new ArrayList<Map<String, Object>>(navValues.length);
+		
+		for (NavType nav : navValues) {
+			final Map<String, Object> navItem = new HashMap<String, Object>(FROM.length, 1);
+			switch (nav) {
+				case KUNDEN:
+					navItem.put(ICON, R.drawable.ic_kunden);
+					navItem.put(TEXT, getString(R.string.s_nav_kunden));
+					break;
+//				
+//				case BESTELLUNGEN:
+//					navItem.put(ICON, R.drawable.ic_bestellungen);
+//					navItem.put(TEXT, getString(R.string.s_nav_bestellungen));
+//					break;
+//					
+				case ARTIKEL:
+					navItem.put(ICON, R.drawable.ic_artikel);
+					navItem.put(TEXT, getString(R.string.s_nav_artikel));
+					break;
+					
+				default:
+					Log.e(LOG_TAG, nav.toString() + " wird nicht in die Navigationsleiste eingetragen");
+					continue;
+			}
+			navList.add(navItem);
+		}
+		
+		final ListAdapter listAdapter = new SimpleAdapter(getActivity(), navList, R.layout.nav_item, FROM, TO);
+		return listAdapter;
+    }
+	
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+        getListView().setOnItemClickListener(this);
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> adapterView, View view, int itemPosition, long itemId) {	
+		Log.d(LOG_TAG, "itemPosition = " + itemPosition);
+		
+		PopupMenu popup;
+		switch (NavType.valueOf(itemPosition)) {
+			case KUNDEN:
+				if (kundenPopup == null) {
+					kundenPopup = new PopupMenu(getActivity(), view);
+					kundenPopup.inflate(R.menu.kunden_popup);
+					kundenPopup.setOnMenuItemClickListener(this);
+				}
+				popup = kundenPopup;
+				break;
+				
+			case ARTIKEL:
+				if (artikelPopup == null) {
+					artikelPopup = new PopupMenu(getActivity(), view);
+					artikelPopup.inflate(R.menu.artikel_popup);
+					artikelPopup.setOnMenuItemClickListener(this);
+				}
+				popup = artikelPopup;
+				break;
+				
+			default:
+				return;
+		}
+		popup.show();
+	}
+	
+	@Override
+	public boolean onMenuItemClick(MenuItem item) {
+		Fragment neuesFragment;
+		switch (item.getItemId()) {
+			case R.id.kunden_suche_id:
+				neuesFragment = new KundeSuchenId();
+				break;
+
+			case R.id.kunden_suche_nachname:
+				neuesFragment = new KundenSucheNachname();
+				break;
+
+			case R.id.kunde_delete:
+				neuesFragment = new KundeDelete();
+				break;
+//
+//			case R.id.kunde_create:
+//				neuesFragment = new KundeCreate();
+//				break;
+//			
+			case R.id.artikel_suche_id:
+				neuesFragment = new ArtikelSuchenId();
+				break;
+				
+			case R.id.artikel_suche_bezeichnung:
+				neuesFragment = new ArtikelSuchenBezeichnung();
+				break;	
+				
+			case R.id.artikel_suche_artikelgruppe:
+				neuesFragment = new ArtikelSuchenArtikelgruppe();
+				break;
+				
+			case R.id.artikel_delete:
+				neuesFragment = new ArtikelDelete();
+				break;
+				
+			case R.id.artikel_create:
+				neuesFragment = new ArtikelCreate();
+				break;
+
+			default:
+				return false;
+		}
+		
+		getFragmentManager().beginTransaction()
+		                    .replace(R.id.details, neuesFragment)
+		                    .addToBackStack(null)  
+		                    .commit();
+		
+		return true;
+	}
+}
